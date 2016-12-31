1d1f4d15ed2e6766ad572e102ffa8b3e8fb6153f
Tanay Singhal <singhal.tanay@gmail.com>
2016-12-31 15:38:43 -0500

    Added temporary usernames

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
   //res.send('<h1> Hello world </h1>');
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  //console.log('a user connected');
+  var username = "user_" + (Math.floor(Math.random()*90000) + 10000).toString();
+
+  io.emit("connected", username);
+
+  socket.on("chat message", function(msg){
+    io.emit("chat message", username + ": " + msg);
+  });
+
+  socket.on('disconnect', function(){
+    io.emit("disconnected", username);
+  });
+
 });
 
 var port = 3000;
